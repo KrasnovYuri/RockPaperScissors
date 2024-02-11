3f46a77094588eac5354396c891232e85ea2c182
KrasnovYuri <krasnov1yuri@gmail.com>
2024-02-11 16:44:44 +0300

    base logic

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+
+    let items = ["Rock👊🏻", "Paper🫳🏻", "Scissors✌🏻"]
+    
+    @State private var computerChoice = ""
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Computer choice \(computerChoice)")
+            
+            ForEach(0..<items.count, id: \.self) { item in
+                Button {
+                    buttonTapped(item)
+                } label: {
+                    Text(items[item])
+                        .font(.system(size: 50))
+                        .shadow(radius: 10)
+                }
+            }
+            Text("Score \(score)")
+                .shadow(radius: 10)
         }
         .padding()
+    }
+    
+    func buttonTapped(_ item: Int) {
+        let userChoice = items[item]
+        computerChoice = items.randomElement() ?? ""
+        
+        switch userChoice {
+        case "Rock👊🏻":
+            if computerChoice == "Scissors✌🏻" {
+                score += 1
+            } else if computerChoice == "Paper🫳🏻" {
+                score -= 1
+            }
+        case "Paper🫳🏻":
+            if computerChoice == "Rock👊🏻" {
+                score += 1
+            } else if computerChoice == "Scissors✌🏻" {
+                score -= 1
+            }
+        case "Scissors✌🏻":
+            if computerChoice == "Paper🫳🏻" {
+                score += 1
+            } else if computerChoice == "Rock👊🏻" {
+                score -= 1
+            }
+        default:
+            print("default case")
+        }
     }
 }
 
